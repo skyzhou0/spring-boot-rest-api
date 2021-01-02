ae2f9ddf063f0d774519f7c719c39b8bf97649bf
skyzhou0 <hao.s.zhou@outlook.com>
2021-01-02 16:26:36 +0000

    creat topic service class

--- src/main/java/io/cambridage/aiml/springboot/topic/TopicController.java
+++ src/main/java/io/cambridage/aiml/springboot/topic/TopicController.java
@@ -3,20 +3,34 @@ package io.cambridage.aiml.springboot.topic;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
+	
+	// Get - Testing API.
 	@RequestMapping("/topic")
 	public String getAllTopics() {
 		return "All Topics";
 	}
 	
+	// Get - Topics (using Services).
+	@Autowired
+	private TopicService topicService;
+	
 	@RequestMapping("/topicjson")
 	public List<Topic> getAllTopicsJson() {
-		return Arrays.asList(
-				new Topic("Spring", "Sring Boot", "Spring is a Java Framework")
-				);
+		
+		return topicService.getAllTopics();
+	}
+
+	// Get - Topic ID (using Services).	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+		
 	}
 }
---
+++ src/main/java/io/cambridage/aiml/springboot/topic/TopicService.java
@@ -0,0 +1,23 @@
+package io.cambridage.aiml.springboot.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	private List<Topic> topics = Arrays.asList(
+			new Topic("Spring", "Sring Boot", "Spring is a Java Framework"),
+			new Topic("Java", "Java EE", "Java EE Framework")
+			);
+	
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+}
